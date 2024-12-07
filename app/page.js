c5075e001c614e21ac8167644e5c704a961ae9ea
JessiP23 <jessi316866@gmail.com>
2024-12-06 23:09:44 -0500

    translations

--- app/page.js
+++ app/page.js
@@ -397,8 +397,8 @@ function Home() {
               >
                 <div className="p-6 bg-gradient-to-r from-[#2c1810] to-[#3f3def] text-white group">
                   <h3 className="text-2xl font-bold mb-2 transition-colors 
-                    group-hover:text-[#dbf240]">{video.title}</h3>
-                  <p className="text-gray-200 line-clamp-3">{video.description}</p>
+                    group-hover:text-[#dbf240]">{translations[language].videoData[index].title}</h3>
+                  <p className="text-gray-200 line-clamp-3">{translations[language].videoData[index].description}</p>
                 </div>
                 <div className="relative group">
                   <div className="relative w-full pt-[75%]"> {/* 4:3 Aspect Ratio */}
